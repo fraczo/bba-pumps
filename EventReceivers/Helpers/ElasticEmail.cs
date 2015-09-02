19da71fcd2dbb538157419876b940fe9f5f1cfb9
fraczo <jacek.rawiak@hotmail.com>
2015-09-02 09:00:38 +0200

    published

---
+++ EventReceivers/Helpers/ElasticEmail.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Collections.Specialized;
+
+namespace Helpers
+{
+    public class ElasticMail
+    {
+        public static string ReportError(Exception ex, string webUrl)
+        {
+            string subject = string.Format(":: ERR :: {0}", webUrl);
+            StringBuilder sb = new StringBuilder();
+            sb.Append(@"<table>");
+            sb.AppendFormat(@"<tr valign='top'><td>{0}</td><td>{1}</td></tr>", "Source", ex.Source);
+            sb.AppendFormat(@"<tr valign='top'><td>{0}</td><td>{1}</td></tr>", "TargetSite", ex.TargetSite);
+            sb.AppendFormat(@"<tr valign='top'><td>{0}</td><td>{1}</td></tr>", "Message", ex.Message);
+            sb.AppendFormat(@"<tr valign='top'><td>{0}</td><td>{1}</td></tr>", "StackTrace", ex.StackTrace);
+            sb.AppendFormat(@"<tr valign='top'><td>{0}</td><td>{1}</td></tr>", "InnerException", ex.InnerException);
+            sb.AppendFormat(@"<tr valign='top'><td>{0}</td><td>{1}</td></tr>", "HelpLink", ex.HelpLink);
+            sb.Append(@"</table>");
+
+            return SendTestEmail(subject, sb.ToString());
+        }
+        
+        public static string SendTestEmail(string subject, string bodyHtml)
+        {
+            //e9b604ee-e197-44a3-a0fa-6b23cf9ec0bb - profil: stafix24@hotmail.com
+            //46779a46-20ab-4670-99b8-b44a9a6f45b5 - profil: biuro@rawcom24.pl
+
+            //SPSecurity.RunWithElevatedPrivileges(delegate()
+            //{
+
+
+            //});
+
+            string USERNAME = "e9b604ee-e197-44a3-a0fa-6b23cf9ec0bb";
+            string API_KEY = "e9b604ee-e197-44a3-a0fa-6b23cf9ec0bb";
+
+            string from = "mailer@stafix24.pl";
+            string fromName = "STAFix24 Mailer";
+            string to = "jacek.rawiak@hotmail.com";
+            string bodyText = "Text Body";
+
+            WebClient client = new WebClient();
+            NameValueCollection values = new NameValueCollection();
+            values.Add("username", USERNAME);
+            values.Add("api_key", API_KEY);
+            values.Add("from", from);
+            values.Add("from_name", fromName);
+            values.Add("subject", subject);
+            if (bodyHtml != null)
+                values.Add("body_html", bodyHtml);
+            if (bodyText != null)
+                values.Add("body_text", bodyText);
+            values.Add("to", to);
+
+            byte[] response = client.UploadValues("https://api.elasticemail.com/mailer/send", values);
+            return Encoding.UTF8.GetString(response);
+        }
+
+    }
+}
---
+++ EventReceivers/tabOfertyHandlowe/tabOfertyHandlowe.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Security;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+using Microsoft.SharePoint.Linq;
+using System.IO;
+using Helpers;
+
+namespace EventReceivers.tabOfertyHandlowe
+{
+    public class tabOfertyHandlowe : SPItemEventReceiver
+    {
+        public override void ItemUpdated(SPItemEventProperties properties)
+        {
+            Execute(properties);
+        }
+
+        private void Execute(SPItemEventProperties properties)
+        {
+            this.EventFiringEnabled = false;
+
+            try
+            {
+
+                SPListItem item = properties.ListItem;
+
+                if (item["colStatus"] != null && item["colStatus"].ToString().Equals("Wysłana"))
+                {
+                    DateTime dataWyslania = item["colDataWyslania"] != null ? DateTime.Parse(item["colDataWyslania"].ToString()) : new DateTime();
+
+                    if (dataWyslania != new DateTime())
+                    {
+
+                        if (item["selZadania"] != null)
+                        {
+
+                            SPFieldLookupValue zadanie = new SPFieldLookupValue(item["selZadania"].ToString());
+
+                            if (zadanie.LookupId > 0)
+                            {
+                                //sprawdź czy którykolwiek z załączonych plików ma w nazwie oferta
+                                const string fileNameMask = "OFERTA";
+
+                                foreach (string attName in item.Attachments)
+                                {
+                                    if (attName.ToUpper().Contains(fileNameMask))
+                                    {
+                                        //sprawdź lokalizację piku źródłowego
+                                        SPFile sFile = properties.Web.GetFile(Files.GetAttachmentUrl(item, attName));
+                                        if (sFile.Exists)
+                                        {
+
+                                            //ustal nazwę docelową (nazwa bieżąca + id oferty + data wysyłki do klienta)
+                                            string name = Path.GetFileNameWithoutExtension(sFile.Name);
+                                            string ext = Path.GetExtension(sFile.Name);
+
+                                            string targetFileName = String.Format("{0}_{1}_{2}{3}",
+                                                name,
+                                                item.ID.ToString(),
+                                                dataWyslania.ToString(),
+                                                ext);
+
+                                            targetFileName = Files.CleanupFileName(targetFileName);
+
+                                            //ustal lokalizację docelową
+                                            SPList destList = properties.Web.Lists.TryGetList("Zadania");
+                                            if (destList != null)
+                                            {
+                                                SPListItem destItem = destList.GetItemById(zadanie.LookupId);
+                                                if (destItem != null)
+                                                {
+                                                    //prawdź czy istnieje
+                                                    string targetUrl = SPUtility.ConcatUrls(destItem.Attachments.UrlPrefix, targetFileName);
+                                                    SPFile targetFile = properties.Web.GetFile(targetUrl);
+                                                    if (targetFile.Exists)
+                                                    {
+                                                        destItem.Attachments.DeleteNow(targetFileName);
+                                                    }
+
+                                                    //skopiuj plik do zadania poo nową nazwą docelową nadpisując istniejący
+                                                    byte[] buffer = sFile.OpenBinary();
+                                                    destItem.Attachments.Add(targetFileName, buffer);
+
+                                                    //uzupełnij pozostałe pola jeżeli są puste
+
+                                                    destItem["colDataWyslaniaOferty"] = dataWyslania;
+                                                    destItem["colWartoscOferty"] = item["colWartoscOferty"] != null ? item["colWartoscOferty"] : 0;
+                                                    destItem.Update();
+                                                }
+                                            }
+                                        }
+
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                var result = ElasticMail.ReportError(ex, properties.WebUrl.ToString());
+            }
+
+            this.EventFiringEnabled = true;
+        }
+
+
+
+
+    }
+}
