d05a4c05b22682771f5289b48b96e2548d9209bd
fraczo <jacek.rawiak@hotmail.com>
2016-03-31 13:21:36 +0200

    Migracja do VS2013

---
+++ Backup/EventReceivers/Helpers/Files.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.SharePoint.Linq;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+
+namespace Helpers
+{
+    public class Files
+    {
+        public static string CleanupFileName(string nazwaPliku)
+        {
+            //string illegal = "\"M\"\\a/ry/ h**ad:>> a\\/:*?\"| li*tt|le|| la\"mb.?";
+            string regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+            Regex r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
+            return r.Replace(nazwaPliku, "");
+        }
+
+        /// <summary>
+        /// How to get attachement URL programatically
+        /// </summary>
+        /// <param name="oItem"></param>
+        /// <returns></returns>
+        //public static string GetAttachmentUrls(Microsoft.SharePoint.SPListItem oItem)
+        //{
+
+        //    string path = string.Empty;
+
+        //    try
+        //    {
+
+        //        path = (from string file in oItem.Attachments
+        //                orderby file
+        //                select SPUrlUtility.CombineUrl(oItem.Attachments.UrlPrefix, file)).FirstOrDefault();
+        //        return path;
+        //    }
+        //    catch
+        //    {
+        //        return string.Empty;
+        //    }
+        //}
+
+        public static string GetAttachmentUrl(Microsoft.SharePoint.SPListItem oItem, string attName)
+        {
+            string url = string.Empty;
+
+            try
+            {
+                url = SPUrlUtility.CombineUrl(oItem.Attachments.UrlPrefix, attName);
+            }
+            catch (Exception)
+            {}
+
+            return url;
+        }
+
+    }
+}
---
+++ Backup/EventReceivers/Helpers/Folders.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint;
+
+namespace EventReceivers.Helpers
+{
+    public class Folders
+    {
+        public static void FolderExists(SPWeb web, string folderName, SPList list, SPListItem listItem)
+        {
+            //Check folder exist in the SPWeb or not 
+
+            SPListItem listFolder = null;
+            bool folderExists = false;
+            // Check to see if folder already exists, if not create it
+            for (int i = 0; i < list.Folders.Count; i++)
+            {
+                if (list.Folders[i].Folder.Name == folderName)
+                {
+                    listFolder = list.Folders[i];
+                    listItem = list.Items.Add(listFolder.Folder.ServerRelativeUrl, SPFileSystemObjectType.File, null);
+                    web.AllowUnsafeUpdates = true;
+                    listItem.Update();
+                }
+            }
+
+            // The folder does not exist so we create it and add the item
+
+            if (!folderExists)
+            {
+                listFolder = list.Items.Add(list.RootFolder.ServerRelativeUrl, SPFileSystemObjectType.Folder, folderName);
+                listFolder.Update();
+                listFolder["Title"] = folderName;
+
+                SPListItem newListItem = list.Items.Add(listFolder.Folder.ServerRelativeUrl, SPFileSystemObjectType.File, null);
+                web.AllowUnsafeUpdates = true;
+                foreach (SPField field in listItem.Fields)
+                {
+                    if (field.ReadOnlyField
+                     || field.Id == SPBuiltInFieldId.Attachments)
+                        continue;
+                    newListItem[field.Id] = listItem[field.Id];
+                }
+
+                newListItem.Update();
+                
+                list.Update();
+            }
+        }
+    }
+}
